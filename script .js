26a5bdfc7b88d37045374c9f1bd4b1b57aaabd2e
arysisley <150989476+arysisley@users.noreply.github.com>
2023-11-16 14:06:46 -0500

    Update script .js

--- script .js
+++ script .js
@@ -1,6 +1,12 @@
 function handleResponse(response) {
     if (response === 'yes') {
-        alert('Congratulations! She said yes!');
+        alert('Congratulations! She said yes! 🎉');
+
+        // Add additional interactions here, such as redirecting or displaying a message.
+        // For example, you can redirect the user to a thank-you page after a delay:
+        setTimeout(function() {
+            window.location.href = 'thankyou.html';
+        }, 3000); // Redirect after 3 seconds
     } else {
         alert('Sorry to hear that. Keep smiling!');
     }
