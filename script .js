53a0e3aef0b3ae1b0d8fc3a17abb2cd6d4607a2b
arysisley <150989476+arysisley@users.noreply.github.com>
2023-11-16 13:53:04 -0500

    Create script .js

---
+++ script .js
@@ -0,0 +1,7 @@
+function handleResponse(response) {
+    if (response === 'yes') {
+        alert('Congratulations! She said yes!');
+    } else {
+        alert('Sorry to hear that. Keep smiling!');
+    }
+}
